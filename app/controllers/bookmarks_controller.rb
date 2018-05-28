42af890c6c6be4fc6708ed5bb7c0cc038181b657
Kerri <kerri.maru@gmail.com>
2018-05-28 18:25:10 +0100

    added name field to user model

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,6 +1,6 @@
 class BookmarksController < ApplicationController
   before_action :set_bookmark, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!
+  
 
   # GET /bookmarks
   # GET /bookmarks.json
