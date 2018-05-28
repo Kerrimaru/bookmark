bbc2af0f641d673138ace3f34789e170c63dcd51
Kerri <kerri.maru@gmail.com>
2018-05-28 13:28:25 +0100

    added some seeds, basic style for bookmarks index view

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_27_105057) do
+ActiveRecord::Schema.define(version: 2018_05_27_120524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.string "url", null: false
+    t.string "screenshot", null: false
+    t.string "title"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_bookmarks_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,4 +42,5 @@ ActiveRecord::Schema.define(version: 2018_05_27_105057) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookmarks", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+@bella = User.create!(email: 'bella@dog.com', password: 'pass123')
+@vik = User.create!(email: 'vik@dog.com', password: 'pass123')
+@bunny = User.create!(email: 'bunbun@dog.com', password: 'pass123')
+
+@bella.bookmarks.create!(
+  url: 'https://kerrimaru.github.io/#main',
+  screenshot: 'tempScreen.png',
+  title: 'Really cool site!!'
+)
+
+@bella.bookmarks.create!(
+  url: 'http://blogpaws.com',
+  screenshot: 'tempScreen.png',
+  title: 'blogpaws'
+)
+
+@bella.bookmarks.create!(
+  url: 'http://www.allthingsdogblog.com',
+  screenshot: 'tempScreen.png',
+  title: 'dogggs'
+)
+
+@bella.bookmarks.create!(
+  url: 'http://www.bringingupbella.com',
+  screenshot: 'tempScreen.png',
+  title: 'A dog named bella!'
+)
+# Bookmark.create!(
+#   user: @bella,
+#   url: 'https://kerrimaru.github.io/#main',
+#   screenshot: '/app/assets/images/tempScreen.png',
+#   title: 'cool site'
+# )
