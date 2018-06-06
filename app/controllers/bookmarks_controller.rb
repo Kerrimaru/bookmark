aebcdaf905795ecef5814390b87c9e54c4f49798
Kerri <kerri.maru@gmail.com>
2018-06-06 10:50:56 +0100

    disable authenticity token

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,6 @@
 class BookmarksController < ApplicationController
   before_action :set_bookmark, only: [:show, :edit, :update, :destroy]
+  skip_before_action: :verify_authenticity_token
   
 
   # GET /bookmarks
